7725590a22d8d346373f55c2c968bd9f1c905f39
Igor Nikolaychuk <jmpeax@icloud.com>
2018-04-18 02:30:56 +0300

    test version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+sleep 10 #Wait for mongod to start
+
+FILENAME=$(date +%Y_%m_%d__%H_%M).tar.gz
+echo "Dumping from "$MONGODB_HOST" database "$DB
+
+while [[ true ]]; do
+	mongodump --host $MONGODB_HOST --gzip --archive=$FILENAME
+	
+	COMMAND="${UPLOAD_COMMAND} ${FILENAME}"
+	echo $COMMAND
+	eval $COMMAND
+	rm $FILENAME
+
+	sleep $BACKUP_INTEVAL;
+done
